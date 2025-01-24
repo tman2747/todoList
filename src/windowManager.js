b1fc0aa3244f697f25d576a0ddf707705c8ad331
Triston Land <tristonland@gmail.com>
2025-01-24 03:48:49 -0700

    add color changing to circles on window manager

--- src/windowManager.js
+++ src/windowManager.js
@@ -3,12 +3,12 @@ import { project } from "./project"
 const content = document.querySelector(".content")
 
 
-export function createrProjectWindow(Project)
+export function createrProjectWindow(mainProject)
 {
     content.innerHTML = ""
     const header = document.createElement("div")
     header.classList.add("header")
-    header.innerHTML = Project.getProjectName()
+    header.innerHTML = mainProject.getProjectName()
     const addButton = document.createElement("div")
     addButton.classList.add("add")
     addButton.innerHTML = "+"
@@ -17,19 +17,18 @@ export function createrProjectWindow(Project)
         //call create popup window here
         console.log("add button clicked")
     })
-
     header.appendChild(addButton)
+    content.appendChild(header)    
 
 
     const cardContainer = document.createElement("div")
     cardContainer.classList.add("card-container")
-        
-        for (let i = 0; i < Project.getProjectList().length; i++)
+    let color = ["blue","red","yellow","green"]
+        for (let i = 0; i < mainProject.getProjectList().length; i++)
             {
-                console.log(i)
                 const card = document.createElement("div")
                 card.classList.add("card")
-                if (i == 0 && i == Project.getProjectList().length)
+                if (i == 0 && i == mainProject.getProjectList().length)
                 {
                     card.id = "first"
                     card.id = "last"
@@ -38,32 +37,30 @@ export function createrProjectWindow(Project)
                 {
                     card.id = "first"
                 }
-                else if (i == Project.getProjectList().length -1)
+                else if (i == mainProject.getProjectList().length -1)
                 {
                     card.id = "last"
                 }
                 const circle = document.createElement("div")
                 circle.classList.add("circle")
+                circle.style.backgroundColor=(color[i % color.length]) // modulo loops through the color array cyclically
                 card.appendChild(circle)
         
                 const cardText = document.createElement("div")
                 cardText.classList.add("card-text")
-                cardText.innerHTML = Project.getProjectList()[i].getProjectName()
+                cardText.innerHTML = mainProject.getProjectList()[i].getProjectName()
+                card.addEventListener("click", ()=>{
+                    // call opening the list view from here
+                })
         
                 card.appendChild(cardText)
                 cardContainer.appendChild(card)
             }
 
-    content.appendChild(header)    
     content.appendChild(cardContainer)
     
 }
 
-
-export function updateWindow(list)
-{
-
-}
 export function createPopupWindow(list)
 {
 
